a551520d5610452d0553c3284bbc5aab4e30c436
Oleg Isanbaev <ingen.ole@yandex.ru>
2024-05-08 00:05:06 +0300

    Add filepaths parameters and format option annotations

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,6 +10,20 @@ import java.io.File;
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference")
 public class App {
+    private static final String DEFAULT_FORMAT = "stylish";
+
+    @Option(
+            names = {"-f", "--format"},
+            defaultValue = DEFAULT_FORMAT,
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format = DEFAULT_FORMAT;
+
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int exitCode = new CommandLine(new App()).execute(args);
