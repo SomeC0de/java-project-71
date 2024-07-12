d5644d7c188c078eebe853031c03d8550858c656
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-07-12 11:04:47 +0300

    Fix @Command content

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,11 +5,14 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
+import java.io.PrintWriter;
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true,
-        description = "Compares two configuration files and shows a difference")
+@Command(name = "gendiff",
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference",
+        mixinStandardHelpOptions = true,
+        exitCodeOnInvalidInput = 123)
 public class App implements Callable<Integer> {
     private static final String DEFAULT_FORMAT = "stylish";
 
