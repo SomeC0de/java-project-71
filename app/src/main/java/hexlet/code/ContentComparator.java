178f7b8ef38b1887b0eef023342454c057fe01b4
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-07-18 11:31:51 +0300

    Implement content comparison

--- app/src/main/java/hexlet/code/ContentComparator.java
+++ app/src/main/java/hexlet/code/ContentComparator.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
-import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ContentComparator {
     public static Map<String, CompareEntity> compare (Map<String, Object> from, Map<String, Object> to) {
@@ -11,6 +9,23 @@ public class ContentComparator {
         keys.addAll(from.keySet());
         keys.addAll(to.keySet());
 
-        return Map.of();
+        return keys.stream().collect(Collectors.toMap(key -> key.toString(), key -> {
+                    if (from.containsKey(key) && !to.containsKey(key)) {
+                        return new CompareEntity(EntityState.REMOVED, from.get(key));
+                    }
+                    else  if (!from.containsKey(key) && to.containsKey(key)) {
+                        return new CompareEntity(EntityState.ADDED, to.get(key));
+                    }
+                    else  if (from.containsKey(key) && to.containsKey(key)) {
+                        if (Objects.equals(from.get(key), to.get(key))) {
+                            return new CompareEntity(EntityState.NOT_CHANGED, from.get(key));
+                        } else {
+                            return new CompareEntity(EntityState.CHANGED, from.get(key), to.get(key));
+                        }
+                    }
+                    else {
+                        throw new RuntimeException("Error: Unknown key found!");
+                    }
+                }, (existing, replacement) -> existing, TreeMap::new));
     }
 }
