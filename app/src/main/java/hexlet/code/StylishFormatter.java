92bd6c54357a278763bcc53dcf9017b2a9f4de83
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-07-19 11:42:05 +0300

    Implement stylish formatter

--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -2,10 +2,47 @@ package hexlet.code;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StylishFormatter {
+    private final static String MARK_UNCHANGED = "  ";
+    private final static String MARK_ADDED = "+ ";
+    private final static String MARK_REMOVED = "- ";
     public static String format(Map<String, CompareEntity> compared) {
-        // TBD:
-        return "";
+        StringBuilder result = new StringBuilder("{\n");
+
+        String formated = compared.keySet().stream()
+                .map(key -> {
+                    CompareEntity record =  compared.get(key);
+                    StringBuilder s = new StringBuilder("\t");
+
+                    switch (record.getEntityState()) {
+                        default:
+                            throw new RuntimeException("Unknown record state!");
+
+                        case NOT_CHANGED:
+                            s.append(MARK_UNCHANGED + key + " : " + record.getValueFrom());
+                            break;
+
+                        case CHANGED:
+                            s.append(MARK_REMOVED + key + " : " + record.getValueFrom() + "\n");
+                            s.append("\t" + MARK_ADDED + key + " : " +  record.getValueTo());
+                            break;
+
+                        case ADDED:
+                            s.append(MARK_ADDED + key + " : " +  record.getValueTo());
+                            break;
+
+                        case REMOVED:
+                            s.append(MARK_REMOVED + key + " : " + record.getValueFrom());
+                            break;
+                    }
+                    return s;
+                }).collect(Collectors.joining("\n"));
+
+        result.append(formated);
+        result.append("\n}\n");
+
+        return result.toString();
     }
 }
