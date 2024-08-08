562f0e54347a2779ea2c8eee70389fb3f6a10a79
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-08 12:18:16 +0300

    Fix linter errors

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,7 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class AppTest {
     @Test
