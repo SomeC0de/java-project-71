1506947aee42accef72c62e287d8f21dc2b0c3f7
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-09 14:06:44 +0300

    Implement simple test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,10 +2,26 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class AppTest {
+    private static String diffFlat;
+
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        diffFlat = Files.readString(Paths.get("src/test/resources/diffFlat"));
+    }
     @Test
     public void dummyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void stylishJsonTest() throws IOException {
+        String actual = Differ.generate("src/test/resources/flat_list0.json",
+                "src/test/resources/flat_list1.json", "stylish");
     }
 }
