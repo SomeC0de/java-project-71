513f6a3395adeb08769c7829f2de16aa29f8de22
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-14 11:44:57 +0300

    Fix Formatter: replace tab with spaces; introduce String constants

--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -7,33 +7,35 @@ public class StylishFormatter {
     private static final String MARK_UNCHANGED = "  ";
     private static final String MARK_ADDED = "+ ";
     private static final String MARK_REMOVED = "- ";
+    private static final String INDENT = "  ";
+    private static final String SEPARATOR = ": ";
     public static String format(Map<String, CompareEntity> compared) {
         StringBuilder result = new StringBuilder("{\n");
 
         String formated = compared.keySet().stream()
                 .map(key -> {
                     CompareEntity record =  compared.get(key);
-                    StringBuilder s = new StringBuilder("\t");
+                    StringBuilder s = new StringBuilder();
 
                     switch (record.getEntityState()) {
                         default:
                             throw new RuntimeException("Unknown record state!");
 
                         case NOT_CHANGED:
-                            s.append(MARK_UNCHANGED + key + ": " + record.getValueFrom());
+                            s.append(INDENT + MARK_UNCHANGED + key + SEPARATOR + record.getValueFrom());
                             break;
 
                         case CHANGED:
-                            s.append(MARK_REMOVED + key + ": " + record.getValueFrom() + "\n");
-                            s.append("\t" + MARK_ADDED + key + ": " +  record.getValueTo());
+                            s.append(INDENT + MARK_REMOVED + key + SEPARATOR + record.getValueFrom() + "\n");
+                            s.append(INDENT + MARK_ADDED + key + SEPARATOR +  record.getValueTo());
                             break;
 
                         case ADDED:
-                            s.append(MARK_ADDED + key + ": " +  record.getValueTo());
+                            s.append(INDENT + MARK_ADDED + key + SEPARATOR +  record.getValueTo());
                             break;
 
                         case REMOVED:
-                            s.append(MARK_REMOVED + key + ": " + record.getValueFrom());
+                            s.append(MARK_REMOVED + key + SEPARATOR + record.getValueFrom());
                             break;
                     }
                     return s;
