521c2bac167f8e085fa6da2596b8bfa13966acc4
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-14 11:25:22 +0300

    Add test sources for nested jsons

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -6,23 +6,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
     private static String diffFlat;
+    private static String diffNested;
 
     @BeforeEach
     public void beforeEach() throws IOException {
         diffFlat = Files.readString(Paths.get("src/test/resources/diffFlat"));
+        diffNested = Files.readString(Paths.get("src/test/resources/diffNested"));
     }
 
     @Test
     public void stylishJsonTest() throws IOException {
         String actual = Differ.generate("src/test/resources/flat_list0.json",
                 "src/test/resources/flat_list1.json", "stylish");
+        assertEquals(actual, diffFlat);
     }
 
     @Test
     public void stylishYmlTest() throws IOException {
         String actual = Differ.generate("src/test/resources/flat_list0.yml",
                 "src/test/resources/flat_list1.yml", "stylish");
+        assertEquals(actual, diffFlat);
+    }
+
+    @Test
+    public void stylishJsonNestedTest() throws IOException {
+        String actual = Differ.generate("src/test/resources/nested_0.json",
+                "src/test/resources/nested_1.json", "stylish");
+        assertEquals(actual, diffNested);
     }
 }
