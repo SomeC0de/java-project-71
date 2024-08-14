e8e65493199a7aba6afafffc8d5d86da55ef300d
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-14 13:27:55 +0300

    Refactor StylishFormatter class

--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -1,48 +1,35 @@
 package hexlet.code;
 
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.StringJoiner;
 
 public class StylishFormatter {
-    private static final String MARK_UNCHANGED = "  ";
-    private static final String MARK_ADDED = "+ ";
-    private static final String MARK_REMOVED = "- ";
-    private static final String INDENT = "  ";
-    private static final String SEPARATOR = ": ";
     public static String format(Map<String, CompareEntity> compared) {
-        StringBuilder result = new StringBuilder("{\n");
+        final StringJoiner result = new StringJoiner("\n", "{\n", "\n}");
 
-        String formated = compared.keySet().stream()
-                .map(key -> {
-                    CompareEntity record =  compared.get(key);
-                    StringBuilder s = new StringBuilder();
+        compared.forEach((key, value) -> {
+            switch (value.getEntityState()) {
+                case NOT_CHANGED:
+                    result.add(String.format("    %s: %s", key, value.getValueFrom()));
 
-                    switch (record.getEntityState()) {
-                        default:
-                            throw new RuntimeException("Unknown record state!");
+                case CHANGED:
+                    result.add(String.format("  - %s: %s", key, value.getValueFrom()));
+                    result.add(String.format("  + %s: %s", key, value.getValueTo()));
+                    break;
 
-                        case NOT_CHANGED:
-                            s.append(INDENT + MARK_UNCHANGED + key + SEPARATOR + record.getValueFrom());
-                            break;
+                case ADDED:
+                    result.add(String.format("  + %s: %s", key, value.getValueTo()));
+                    break;
 
-                        case CHANGED:
-                            s.append(INDENT + MARK_REMOVED + key + SEPARATOR + record.getValueFrom() + "\n");
-                            s.append(INDENT + MARK_ADDED + key + SEPARATOR +  record.getValueTo());
-                            break;
+                case REMOVED:
+                    result.add(String.format("  - %s: %s", key, value.getValueFrom()));
+                    break;
 
-                        case ADDED:
-                            s.append(INDENT + MARK_ADDED + key + SEPARATOR +  record.getValueTo());
-                            break;
+                default:
+                    throw new RuntimeException("Unknown record state!");
 
-                        case REMOVED:
-                            s.append(MARK_REMOVED + key + SEPARATOR + record.getValueFrom());
-                            break;
-                    }
-                    return s;
-                }).collect(Collectors.joining("\n"));
-
-        result.append(formated);
-        result.append("\n}");
+            }
+        });
 
         return result.toString();
     }
