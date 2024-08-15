72a1113f3d102bfa113e7f218bb400e2224a1ad5
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-15 09:48:24 +0300

    Remove excessive class

--- app/src/main/java/hexlet/code/EntityState.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code;
-
-// TBD: replace consts to CompareEntity class as String
-public enum EntityState {
-    NOT_CHANGED,
-    CHANGED,
-    REMOVED,
-    ADDED
-}
--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -3,6 +3,14 @@ package hexlet.code;
 import java.util.Map;
 import java.util.StringJoiner;
 
+// TBD: replace consts to CompareEntity class as String
+enum EntityState {
+    NOT_CHANGED,
+    CHANGED,
+    REMOVED,
+    ADDED
+}
+
 public class StylishFormatter {
     public static String format(Map<String, CompareEntity> compared) {
         final StringJoiner result = new StringJoiner("\n", "{\n", "\n}");
