06a50883a65fe83966f468b1e943c4ab5dc5515a
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-15 10:07:58 +0300

    Bind report generation to record state

--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -5,10 +5,32 @@ import java.util.StringJoiner;
 
 // TBD: replace consts to CompareEntity class as String
 enum EntityState {
-    NOT_CHANGED,
-    CHANGED,
-    REMOVED,
-    ADDED
+    NOT_CHANGED {
+        public String generate(String key, Object valueFrom, Object valueTo) {
+            return String.format("    %s: %s", key, valueFrom);
+        }
+    },
+    CHANGED {
+        public String generate(String key, Object valueFrom, Object valueTo) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("  - %s: %s\n", key, valueFrom));
+            sb.append(String.format("  + %s: %s", key, valueTo));
+
+            return sb.toString();
+        }
+    },
+    REMOVED {
+        public String generate(String key, Object valueFrom, Object valueTo) {
+            return String.format("  - %s: %s", key, valueFrom);
+        }
+    },
+    ADDED {
+        public String generate(String key, Object valueFrom, Object valueTo) {
+            return String.format("  + %s: %s", key, valueTo);
+        }
+    };
+
+    public abstract String generate(String key, Object valueFrom, Object valueTo);
 }
 
 public class StylishFormatter {
@@ -17,13 +39,14 @@ public class StylishFormatter {
 
         compared.forEach((key, value) -> {
             switch (value.getEntityState()) {
-                case NOT_CHANGED -> result.add(String.format("    %s: %s", key, value.getValueFrom()));
-                case CHANGED -> {
-                    result.add(String.format("  - %s: %s", key, value.getValueFrom()));
-                    result.add(String.format("  + %s: %s", key, value.getValueTo()));
-                }
-                case ADDED -> result.add(String.format("  + %s: %s", key, value.getValueTo()));
-                case REMOVED -> result.add(String.format("  - %s: %s", key, value.getValueFrom()));
+                case NOT_CHANGED ->
+                        result.add(EntityState.NOT_CHANGED.generate(key, value.getValueFrom(), value.getValueTo()));
+                case CHANGED ->
+                        result.add(EntityState.CHANGED.generate(key, value.getValueFrom(), value.getValueTo()));
+                case ADDED ->
+                        result.add(EntityState.ADDED.generate(key, value.getValueFrom(), value.getValueTo()));
+                case REMOVED ->
+                        result.add(EntityState.REMOVED.generate(key, value.getValueFrom(), value.getValueTo()));
                 default -> throw new RuntimeException("Unknown record state!");
             }
         });
