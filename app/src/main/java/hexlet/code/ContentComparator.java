5998129af31e186013929bbe3c08ede5fa3f9090
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-08-22 15:18:01 +0300

    WIP: refactor ContentComparator class

--- app/src/main/java/hexlet/code/ContentComparator.java
+++ app/src/main/java/hexlet/code/ContentComparator.java
@@ -1,16 +1,48 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.TreeMap;
 
 public class ContentComparator {
     public static Map<String, CompareRecord> compare(Map<String, Object> from, Map<String, Object> to) {
         var keys = new TreeSet<>();
         keys.addAll(from.keySet());
         keys.addAll(to.keySet());
+
+        List<Map<String, Object>> compared = new ArrayList<Map<String, Object>>();
+
+        compared = keys.stream().map(key -> {
+            if (from.containsKey(key.toString()) && !to.containsKey(key.toString())) {
+                return new LinkedHashMap<String, Object>(){{
+                    put("key", key.toString());
+                    put("state", RecordStatus.REMOVED);
+                    put("value", from.get(key.toString()));
+                }};
+            } else  if (!from.containsKey(key.toString()) && to.containsKey(key.toString())) {
+                return new LinkedHashMap<String, Object>(){{
+                    put("key", key.toString());
+                    put("state", RecordStatus.ADDED);
+                    put("value", to.get(key.toString()));
+                }};
+            } else if (from.containsKey(key.toString()) && to.containsKey(key.toString())) {
+                if (Objects.equals(from.get(key.toString()), to.get(key.toString()))) {
+                    return new LinkedHashMap<String, Object>(){{
+                        put("key", key.toString());
+                        put("state", RecordStatus.NOT_CHANGED);
+                        put("value", from.get(key.toString()));
+                    }};
+                } else {
+                    return new LinkedHashMap<String, Object>() {{
+                        put("key", key.toString());
+                        put("state", RecordStatus.CHANGED);
+                        put("from", from.get(key.toString()));
+                        put("to", to.get(key.toString()));
+                    }};
+                }
+            } else {
+                throw new RuntimeException("Error: Unknown key found!");
+            }
+        }).collect(Collectors.toList());
 
         return keys.stream().collect(Collectors.toMap(key -> key.toString(), key -> {
             if (from.containsKey(key) && !to.containsKey(key)) {
