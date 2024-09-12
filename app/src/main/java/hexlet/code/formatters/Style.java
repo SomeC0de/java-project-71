63e7f6de36846fc9f71636481c9ea2f3276cfa5f
SomeC0de <ingen.ole@yandex.ru>
2024-09-13 01:55:12 +0400

    Make Style functional interface

--- app/src/main/java/hexlet/code/formatters/Style.java
+++ app/src/main/java/hexlet/code/formatters/Style.java
@@ -3,6 +3,7 @@ package hexlet.code.formatters;
 import java.util.List;
 import java.util.Map;
 
+@FunctionalInterface
 public interface Style {
     String format(List<Map<String, Object>> compared);
 }
