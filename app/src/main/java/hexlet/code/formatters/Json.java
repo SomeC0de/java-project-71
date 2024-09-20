cfbbf7a8130f5e9949e9704f11eca093032af9ed
Oleg Isanbaev <isanbaev@trei.gmbh>
2024-09-20 18:30:14 +0300

    WIP: implement compared data normalisation for JSON

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -2,12 +2,20 @@ package hexlet.code.formatters;
 
 import hexlet.code.builders.JsonBuilder;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public final class Json implements Style {
     @Override
     public String apply(List<Map<String, Object>> compared) {
-        return JsonBuilder.build(compared);
+        List<Map<String, Object>> normalized = compared.stream().map(element -> {
+            Map<String, Object> lowercasedRecord = new LinkedHashMap<>();
+            element.forEach((key, value) -> {
+                lowercasedRecord.put(key.toLowerCase(), value);
+            });
+            return null;
+        }).toList();
+        return JsonBuilder.build(normalized);
     }
 }
